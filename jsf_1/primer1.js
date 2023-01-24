6a1fa1174cbe099eb332de8f58923cb43053578c
Jakicm <miloshj@gmail.com>
2023-01-24 15:43:47 +0100

    EDIT

---
+++ jsf_1/primer1.js
@@ -0,0 +1,13 @@
+/*console.log("Hello World")
+console.warn("Hello World")
+console.info("Hello World")
+console.error("Hello World")*/
+
+let a = 5;
+const b = 7;
+
+console.log(a);
+console.log(b);
+
+a = 8;
+console.log(a);
---
+++ jsf_1/primer6.js
@@ -0,0 +1,5 @@
+const a = "5"
+const b = 7
+
+console.log("Sabiranje:", parseInt(a)+b)
+console.log("Mnozenje:", a*b)
