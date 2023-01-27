33a58919be2b5ea95a66fe28982f3ce698365934
Jakicm <miloshj@gmail.com>
2023-01-27 15:35:12 +0100

    EDIT

---
+++ jsf_3/primer2.js
@@ -0,0 +1,33 @@
+
+
+
+function saberi(a,b) {
+    return a + b
+}
+
+function bacanjeKocke() {
+    const r = Math.random()
+    const r1 = r * 6
+    const r2 = r1 + 1
+    return Math.floor(r2)
+    
+}
+
+console.log("Kocka:", bacanjeKocke() )
+console.log("Kocka:", bacanjeKocke() )
+console.log("Kocka:", bacanjeKocke() )
+console.log("Kocka:", bacanjeKocke() )
+console.log("Kocka:", bacanjeKocke() )
+
+function slucajniBroj(donjaGranica, gornjaGranica) {
+    
+}
+
+const a = 5;
+const y = 6;
+
+const z = saberi(a, y)
+console.log(z)
+
+const z2 = saberi(20,10)
+console.log(z2)
