fd87642864c53325f385a9a8239eae612ce11f0c
Jakicm <miloshj@gmail.com>
2023-02-21 15:43:23 +0100

    EDIT

--- jsf_3/primer3.js
+++ jsf_3/primer3.js
@@ -1,6 +1,26 @@
 
-const pera = {"ime": "Petar", "prezime": "Petrovic", "godiste": 1995 }
+function zaIspis(osoba) {
+    return "Ime: " + osoba.ime + "\n" + 
+    "Prezime: " + osoba.prezime + "\n" +
+    "Godiste: " + osoba.godiste + "\n"
+} 
 
-pera.ime = "Marko" //izmena
+const pera = {
+    "ime": "Petar", 
+    "prezime": "Petrovic", 
+    "godiste": 1995,
+    "mail": "nekit@gmail.com", 
+    "adresa": {
+        "ulica": "Cara Dusana 34",
+        "broj": 34,
+        "grad": "Beograd",
 
-console.log(pera.ime)
+    }
+}
+
+// pera.ime = "Marko" //izmena
+
+// console.log(pera["adresa"]["ulica"])
+
+// console.log(pera.adresa.ulica)
+console.log(zaIspis(pera))
---
+++ jsf_3/primer4.js
@@ -0,0 +1,4 @@
+const nizBrojeva = [6,7,8,1,2,3]
+
+console.log(nizBrojeva[2])
+console.log("Broj elemenata:", nizBrojeva.length)
