28393f28f5274846e97d436cacf49a9e0a15767e
Jakicm <miloshj@gmail.com>
2023-02-27 15:44:36 +0100

    EDIT

--- jsf_3/primer4.js
+++ jsf_3/primer4.js
@@ -2,3 +2,6 @@ const nizBrojeva = [6,7,8,1,2,3]
 
 console.log(nizBrojeva[2])
 console.log("Broj elemenata:", nizBrojeva.length)
+//nizBrojeva.push(4) - dodaje na kraj niza
+//nizBrojeva.unshift(5) - dodaje na pocetak
+console.log("Niz: ", nizBrojeva.toString())
