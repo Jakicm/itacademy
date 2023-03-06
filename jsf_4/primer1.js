889a8c2ffe3e04f01e7533464c8ce0998b0d43e3
Jakicm <miloshj@gmail.com>
2023-03-06 15:26:26 +0100

    EDIT

---
+++ jsf_4/primer1.js
@@ -0,0 +1,15 @@
+const a = 5
+const b = 6
+
+if (a>0) {
+console.log("a je pozitivan broj")
+}
+
+//IF / ELSE GRANA
+
+if (a>b) {
+    console.log("a je veci od B")
+}
+else {
+    console.log("b je veci")
+}
