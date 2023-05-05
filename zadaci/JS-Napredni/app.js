d58830f8930fe41017205e01785244305520d426
Jakicm <miloshj@gmail.com>
2023-05-05 15:06:36 +0200

    edit

--- zadaci/JS-Napredni/app.js
+++ zadaci/JS-Napredni/app.js
@@ -1,19 +1,20 @@
 
 class Student {
-    constructor(name, address, phone) {
+    constructor(name, address, phone, course) {
       this.name = name;
       this.address = address;
       this.phone = phone;
+      this.course = course;
     }
 
     getInfo() {
-        return `Name: ${this.name}\nAddress: ${this.address}\nPhone: ${this.phone}`;
+        return `Name: ${this.name}\nAddress: ${this.address}\nPhone: ${this.phone}\nCourse: ${this.course}`;
     }
     }
 
-    const student1 = new Student("Marko Markovic", "Beogradska 23, Beograd", "+38164640640");
-    const student2 = new Student("Petar Petrovic", "Novosadska 23, Novi Sad", "+38164641641");
-    const student3 = new Student("Milos Milosevic", "Mitrovacka 23, Sr. Mitrovica", "+38164642642");
+    const student1 = new Student("Marko Markovic", "Beogradska 23, Beograd", "+38164640640", "JS");
+    const student2 = new Student("Petar Petrovic", "Novosadska 23, Novi Sad", "+38164641641", "C#");
+    const student3 = new Student("Milos Milosevic", "Mitrovacka 23, Sr. Mitrovica", "+38164642642", "C++");
 
     const students = [student1, student2, student3];
     for (const student of students) {
